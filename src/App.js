68fc6a10ecb2c70f41a15c4ca9a67a3348010985
fin.usman.abid <usman.abid@fyntros.com>
2021-10-07 10:37:14 +0500

    Conditional Rendering of Multiple Role Dashboards and Views

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {DefaultMain} from "./components/Default";
+import {StudentMain} from "./components/Student";
+import {TeacherMain} from "./components/Teacher";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const {role} = {role: "nothing"};
+  return Containers.getView(role)()
+}
+
+const Containers = {
+  student : StudentMain,
+  teacher : TeacherMain,
+  default: DefaultMain,
+  getView: (role) => Containers[role] ? Containers[role] : Containers.default
 }
 
 export default App;
